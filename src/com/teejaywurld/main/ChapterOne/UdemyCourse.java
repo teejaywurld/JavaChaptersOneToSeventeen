ee0296b9de2ce569e8e1fcbff942738eedcdc57a
Adetunji Henry <teejaywurld@gmail.com>
2022-09-01 05:31:56 +0100

    updated code

--- src/com/teejaywurld/main/ChapterOne/UdemyCourse.java
+++ src/com/teejaywurld/main/ChapterOne/UdemyCourse.java
@@ -87,11 +87,11 @@ public class UdemyCourse {
 //      Convert a given number of pounds to kilograms.
 
 //        STEPS:
-//      1. Create a variable with the appropriate type to store the number of         pounds to be converted to kilograms.
-//      2. Calculate the result i.e. the number of kilograms based on the             contents of the variable above and store the result in a second               appropriate variable.
+//      1. Create a variable with the appropriate type to store the number of pounds to be converted to kilograms.
+//      2. Calculate the result i.e. the number of kilograms based on the contents of the variable above and store the result in a second appropriate variable.
 //      3.  Print out the result.
 
-//      HINT: ! pounds is equal to 0.45359237 of a kilogram. This should help         you perform the calculation
+//      HINT: ! pounds is equal to 0.45359237 of a kilogram. This should help you perform the calculation
 
 //        TODO: Challenge Solution
         double poundsValue = 56.78;

d60bb39a55821cc8a500f937dadd35a653ad2277
Adetunji Henry <teejaywurld@gmail.com>
2023-03-08 14:53:27 +0100

    updated commit

---
+++ src/com/teejaywurld/main/ChapterOne/RandomIntegers.java
@@ -0,0 +1,21 @@
+package com.teejaywurld.main.ChapterOne;
+
+import java.security.SecureRandom;
+
+public class RandomIntegers {
+
+    public static void main(String[] args) {
+
+        SecureRandom randomNumbers = new SecureRandom();
+
+        for (int i = 1; i <= 20; i++) {
+            int face = 1 + randomNumbers.nextInt(6);
+
+            System.out.printf("%d ", + face);
+
+            if (i %  5 == 0){
+                System.out.println();
+            }
+        }
+    }
+}

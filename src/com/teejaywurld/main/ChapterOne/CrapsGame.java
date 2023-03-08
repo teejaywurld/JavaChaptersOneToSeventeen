f7068add963808484ee480c15ec73dd528988ab8
Adetunji Henry <teejaywurld@gmail.com>
2023-03-09 00:19:01 +0100

    added info for how the game is played

--- src/com/teejaywurld/main/ChapterOne/CrapsGame.java
+++ src/com/teejaywurld/main/ChapterOne/CrapsGame.java
@@ -1,5 +1,15 @@
 package com.teejaywurld.main.ChapterOne;
 
+// TODO: A popular game of chance is a dice game known as craps. which is played in casinos and
+//  back alleys throughout the world. The rules of the game are straightforward:
+//      You role two dice, Each die has six faces, which contain one, two, three, four, five and
+//      six spots, respectively. After the dice have come to rest, the sum of the spots on the two
+//      upward faces is calculated. if the sum is 7 or 11 on the first throw, you win. If the sum is
+//      2, 3 or 12 on the first throw (called craps), you lose (i.e., the "house" wins). If the
+//      sum is 4, 5, 6, 8, 9 or 10 on the first throw, that sum becomes your "point." To win, you must
+//      continue rolling the dice until you "make your point" (i.e., roll that same point value). You
+//      lose by rolling a 7 before making your point.
+
 import java.security.SecureRandom;
 
 public class CrapsGame {

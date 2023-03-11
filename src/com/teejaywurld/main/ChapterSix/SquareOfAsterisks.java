70b49a086f8358de3a98dcd8ccab18c614b78e61
Adetunji Henry <teejaywurld@gmail.com>
2023-03-11 15:33:07 +0100

    updated commit

---
+++ src/com/teejaywurld/main/ChapterSix/SquareOfAsterisks.java
@@ -0,0 +1,36 @@
+package com.teejaywurld.main.ChapterSix;
+
+// TODO:
+//  (Displaying a Square of Asterisks) Write a method squareOfAsterisks that displays a solid
+//  square (the same number of rows and columns) of asterisks whose side is specified in integer
+//  parameter side. For example, if side is 4, the method should display
+//          ****
+//          ****
+//          ****
+//          ****
+//  Incorporate this method into an application that reads an integer value for side from the user and
+//  outputs the asterisks with the squareOfAsterisks method.
+
+import java.util.Scanner;
+
+public class SquareOfAsterisks {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the side of a square: ");
+        int side = scanner.nextInt();
+        scanner.close();
+
+        squareOfAsterisks(side);
+    }
+
+    private static void squareOfAsterisks(int side) {
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
